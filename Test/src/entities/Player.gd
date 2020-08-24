4815abb93b2e70dc63e826d0ee3bde4a6dfd0f26
Ckurt <kuroysiris@gmail.com>
2020-08-24 19:31:27 +0200

    added double space before ft's

--- Test/src/entities/Player.gd
+++ Test/src/entities/Player.gd
@@ -5,8 +5,10 @@ var maxSpeed : int = 500
 var acceleration : int = 1000
 var motion : Vector2 = Vector2.ZERO
 
+
 func _physics_process(delta: float) -> void:
 	move_player_test(delta)
+
 
 func get_input_axis() -> Vector2:
 	var axis = Vector2.ZERO
@@ -14,16 +16,19 @@ func get_input_axis() -> Vector2:
 	axis.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	return(axis.normalized())
 
+
 func apply_friction(amount) -> void:
 	if(motion.length() > amount):
 		motion -= motion.normalized() * amount
 	else:
 		motion = Vector2.ZERO
 
+
 func apply_movement(acceleration_value) -> void:
 	motion += acceleration_value
 	if(motion.length() > maxSpeed):
 		motion = motion.clamped(maxSpeed)
+
 
 func move_player_test(delta) -> void:
 	var axis = get_input_axis()
@@ -34,6 +39,7 @@ func move_player_test(delta) -> void:
 			motion = motion.clamped(maxSpeed)
 		motion = move_and_slide(axis * speed)
 		print(motion)
+
 
 #Smoother approach, way too slippy for the shooter style
 func move_player(delta):
