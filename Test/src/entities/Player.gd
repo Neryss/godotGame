3b5182cc440bbe1a4da1734ffc429c918390d870
neryss <kuroysiris@gmail.com>
2020-08-24 23:53:04 +0200

    Added sublime text support for mac, cleanded player script

--- Test/src/entities/Player.gd
+++ Test/src/entities/Player.gd
@@ -1,13 +1,13 @@
 extends entity
 
-var speed : int = 400
+var speed : int = 250
 var maxSpeed : int = 500
 var acceleration : int = 1000
 var motion : Vector2 = Vector2.ZERO
 
 
 func _physics_process(delta: float) -> void:
-	move_player_test(delta)
+	move_player_test()
 
 
 func get_input_axis() -> Vector2:
@@ -29,8 +29,8 @@ func apply_movement(acceleration_value) -> void:
 	if(motion.length() > maxSpeed):
 		motion = motion.clamped(maxSpeed)
 
-
-func move_player_test(delta) -> void:
+#
+func move_player_test() -> void:
 	var axis = get_input_axis()
 	if(axis == Vector2.ZERO):
 		motion = Vector2.ZERO
@@ -39,15 +39,3 @@ func move_player_test(delta) -> void:
 			motion = motion.clamped(maxSpeed)
 		motion = move_and_slide(axis * speed)
 		print(motion)
-
-
-#Smoother approach, way too slippy for the shooter style
-func move_player(delta):
-	var axis = get_input_axis()
-	if(axis == Vector2.ZERO):
-		#Don't know if I keep the slow down or not
-		#apply_friction(acceleration * delta)
-		motion = Vector2.ZERO
-	else:
-		apply_movement(axis * acceleration * delta)
-	motion = move_and_slide(motion)
